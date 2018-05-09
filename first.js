3fb89094b6ab9aa591e63bc6875802dd35b9682e
ivanpilipchack <38336001+ivanpilipchack@users.noreply.github.com>
2018-05-09 19:12:20 +0200

    Add files via upload

---
+++ first.js
@@ -0,0 +1,122 @@
+/*$('#clickme').on('click', () => {
+    $('#robot').fadeToggle(2000);
+  });
+$('#clickme2').on('click', () => {
+  $('#anim').slideToggle('slow');
+});
+$('#clickme3').on('click', () =>{
+  $('#auto').toggle('slow');
+
+});
+$('#out').on('click', () =>{
+  $('#man').fadeOut(2000);
+});
+$('#main').css('background', '#FFFFFF');
+//міняє стиль елемента
+//var str1 = "привіт".length;
+//змінна   значення кількість літер 
+//var str2 = "максим".toUpperCase();
+//змінна   значення вивід літер великими
+//var str3 = "як твої".trim();
+//змінна   значення видаляє пробіли
+//var str4 = "справи";
+
+//alert(str1);
+//alert(str2);
+//alert(str3);
+//alert(str4);
+/////the end////
+
+*/
+/*
+$(document).ready(() => {
+  
+  $('.shoe-details').show();
+  
+  $('.more-details-button').on('click', event => {
+    $(event.currentTarget).closest('.product-details').next().toggle()
+  });  
+  
+  $('.shoe-details li').on('click', event => {
+    $(event.currentTarget).addClass('active');
+    
+    $(event.currentTarget).siblings().removeClass('active')
+    
+    $(event.currentTarget).closest('.shoe-details').children().removeClass('disabled')
+    $('.product-detalis').closest('active').next();
+    
+  });
+  
+  
+  
+  ///////////////////////////////////////////
+  $('.login-button').on('click', () => {
+    $('.login-form').slideToggle(1000);
+    $('.login-button').toggleClass('button-active');
+  });
+
+  $('.product-photo').on('mouseenter', () => {
+    $(this).addClass('photo-active');
+  }).on('mouseleave', function() {
+    $(this).removeClass('photo-active');
+  });
+
+  $('.menu-button').on('click', () => {
+    $('.menu-button').slideToggle('button-active');
+    $('.nav-menu').toggleClass('hide');
+  });
+})
+ 
+var myName = "ivan"
+var myAge = 22;
+var myHobby = "history";
+
+myAge++;
+
+alert(myName);
+alert(myAge);
+alert(myHobby);
+
+var LoveHistory = true;
+if (true) {
+  alert("I an Cesar");
+} else {
+  alert("No History Fan");
+}
+
+var myNameIvan = true;
+if (false) {
+  alert("yesss");
+} else if (true)  {
+  alert("nooooo");
+}
+
+
+var iAmActive = false;
+if (false) {
+  alert("God Man");
+} else if (true) {
+  alert("Not God Man");
+}
+
+var studyJava = "Like";
+if(!studyJava) {
+  alert("well bee god programing");
+}
+else {
+  alert("not godd result");
+}
+
+var age = 20;
+if (age === 20) {
+  alert('welcome');
+} else {
+  alert('sorry');
+}
+*/
+var coal = 3;
+ if (coal > 4) {
+  alert('final');
+ } else {
+  alert('home');
+ }
